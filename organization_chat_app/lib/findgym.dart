2fc74bf043ed2742c6286f9f8c7a36dca684ece5
ParkSangBeom <46027250+ParkSangbeomm@users.noreply.github.com>
2021-11-03 22:25:04 +0900

    feat : google map 연결

--- organization_chat_app/lib/findgym.dart
+++ organization_chat_app/lib/findgym.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class FindgymPage extends StatefulWidget {
   @override
@@ -6,13 +9,29 @@ class FindgymPage extends StatefulWidget {
 }
 
 class _FindgymPageState extends State<FindgymPage> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  CameraPosition _currentPosition = CameraPosition(
+    target: LatLng(13.0827, 80.2707), //사용자 지정 좌표
+    zoom: 12, //확대
+  );
+
   @override
   Widget build(BuildContext context) {
-
-    return const Scaffold(
-      body: Center(
-        child: Text('Find gym page',style: TextStyle(color: Color(0xff383434))),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Google Maps"),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: GoogleMap(
+          initialCameraPosition: _currentPosition,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete();
+          },
+        ),
+      ),
     );
   }
 }
