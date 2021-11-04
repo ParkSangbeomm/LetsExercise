9bcd2dd9a3d1f100098d8539c12493bcaf7e12aa
Yeob <happyk100@naver.com>
2021-11-04 22:17:17 +0900

    Create by haerin on 2021/11/04  - chore : mypage.dart

--- organization_chat_app/lib/mypage.dart
+++ organization_chat_app/lib/mypage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyPage extends StatefulWidget {
   @override
@@ -8,11 +9,50 @@ class MyPage extends StatefulWidget {
 class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
+    double swidth = MediaQuery.of(context).size.width;
+    double sheight = MediaQuery.of(context).size.height;
 
-    return const Scaffold(
-      body: Center(
-        child: Text('My page',style: TextStyle(color: Color(0xff383434))),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return Scaffold(
+
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+
+                  // 동그라미
+                  Container(
+                    margin: EdgeInsets.fromLTRB(15, 20, 0, 0),
+                    height: 40,
+                    width: 120,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: const Color(0xffe49191),
+                    ),
+                    child: Center(
+                      child: Text('프로필 정보', ),
+
+                    ),
+                  ),
+                  // 선
+                  Container(
+                    margin: EdgeInsets.only(top: 17),
+                    height: 2.0,
+                    width: swidth*0.6,
+                    color: const Color(0xffe49191),
+                  )
+                ],
+              ),
+
+            ],
+          ),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+
 }
+
