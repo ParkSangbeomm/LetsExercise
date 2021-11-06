ee5b4951276e4a92ff9a9fb9e34431eb4ac0cc6a
ParkSangBeom <46027250+ParkSangbeomm@users.noreply.github.com>
2021-11-06 22:19:29 +0900

    style : add text field and text top of google maps

--- organization_chat_app/lib/findgym.dart
+++ organization_chat_app/lib/findgym.dart
@@ -1,7 +1,8 @@
-import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'locations.dart' as locations;
+
 
 class FindgymPage extends StatefulWidget {
   @override
@@ -9,25 +10,61 @@ class FindgymPage extends StatefulWidget {
 }
 
 class _FindgymPageState extends State<FindgymPage> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  CameraPosition _currentPosition = CameraPosition(
-    target: LatLng(13.0827, 80.2707), //사용자 지정 좌표
-    zoom: 12, //확대
-  );
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getGoogleOffices();
+    setState(() {
+      _markers.clear();
+      for (final office in googleOffices.offices) {
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+        );
+        _markers[office.name] = marker;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        child: GoogleMap(
-          initialCameraPosition: _currentPosition,
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete();
-          },
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.12,
+              child: Column(
+                children: [
+                  SizedBox(height: 10,),
+                  TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Hint',
+                      )
+                  ),
+                  Container(
+                    alignment: Alignment(1.0, 0.0),
+                    child: Text('HI'),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: const CameraPosition(
+                  target: LatLng(35.95, 128.25),
+                  zoom: 2,
+                ),
+                markers: _markers.values.toSet(),
+              ),
+            ),
+          ],
         ),
       ),
     );
