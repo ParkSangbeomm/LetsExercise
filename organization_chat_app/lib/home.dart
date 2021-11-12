fd7ea1ce9632fd5c3678a296f7ccf98e95dbfa07
haerin7427 <43716537+haerin7427@users.noreply.github.com>
2021-11-12 22:21:43 +0900

    Create by haerin on 2021/11/12  - chore : home.dart

--- organization_chat_app/lib/home.dart
+++ organization_chat_app/lib/home.dart
@@ -140,6 +140,15 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                   rankBox(),
+                  // Stack(
+                  //   children: [
+                  //     Container(
+                  //       width: 400,
+                  //       height: 400,
+                  //       color: Colors.green,
+                  //     ),
+                  //   ],
+                  // )
                 ],
               ),
             ),
@@ -203,7 +212,7 @@ class rankBox extends StatelessWidget {
           ),
 
           Container(
-            height: 160,
+            height: 145,
             margin: EdgeInsets.fromLTRB(
                 0.0, 0.0, 0.0, 0.0),
             child: SingleChildScrollView(
