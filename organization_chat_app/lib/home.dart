abc6a373841a33d1d93cd8734241721493fb5787
haerin7427 <43716537+haerin7427@users.noreply.github.com>
2021-11-12 23:03:50 +0900

    Create by haerin on 2021/11/12  - chore : home.dart

--- organization_chat_app/lib/home.dart
+++ organization_chat_app/lib/home.dart
@@ -195,9 +195,6 @@ class rankBox extends StatelessWidget {
                   CircleAvatar(
                     radius: 40,
                     backgroundImage: AssetImage('img/profile.jpeg'),
-                  ),CircleAvatar(
-                    radius: 40,
-                    backgroundImage: AssetImage('img/profile.jpeg'),
                   ),
                   SizedBox(
                     height: MediaQuery.of(context).size.height * 0.01,
