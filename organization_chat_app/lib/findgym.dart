15be8402025fd8a8e345b793d71020a87db2a4b0
Cocomong98 <yonghjjm0618@gmail.com>
2021-11-30 17:38:30 +0900

    c

--- organization_chat_app/lib/findgym.dart
+++ organization_chat_app/lib/findgym.dart
@@ -24,18 +24,37 @@ class _ListGymPageState extends State<ListGymPage> {
                   width:  MediaQuery.of(context).size.width,
                   height: MediaQuery.of(context).size.height * 0.2,
                   color: const Color(0xffe49191),
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       SizedBox(height: MediaQuery.of(context).size.height*0.05,),
-                      Text('서울시 중구 태평'),
-                      Text('위치변경'),
-                      IconButton(
-                        icon: Icon(Icons.assistant_navigation),
-                        onPressed: (){
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => FindgymPage()));
-                        },
+                      Text('서울 중구 태평로1가'
+                       ,style: TextStyle(
+                       fontWeight: FontWeight.bold,
+                       fontSize: 25,
+                       color: Colors.black,
+                       ),
+                      ),
+                      Text('위치변경'
+                        ,style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 15,
+                          color: Colors.grey,
+                        ),),
+                      Row(
+                        children: [
+                          SizedBox(width:MediaQuery.of(context).size.width*0.8,),
+                          IconButton(
+                            icon: Icon(Icons.assistant_navigation),
+                            onPressed: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context) => FindgymPage()));
+                            },
+                          ),
+                        ],
                       ),
                     ],
+
                   ),
                 ),
                 Expanded(
@@ -54,16 +73,37 @@ class _ListGymPageState extends State<ListGymPage> {
                                   Navigator.push(context, MaterialPageRoute(builder: (context) => DetailGym()));
                                 },
                                 child: Image.asset('img/profile.jpeg',width : MediaQuery.of(context).size.width*0.9,height:MediaQuery.of(context).size.height * 0.2,fit: BoxFit.fitWidth,)),
-                            Column(
-                              children: [
-                              Text('을지로 피트니스 101'),
-                              Row(
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(10,17,10,17),
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text('서울 중구 저동1가'),
-                                  Text('0.8km')
-                                ],
+                                Text('을지로 피트니스 101'
+                                  ,style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17,
+                                    color: Colors.black,
+                                  ),),
+                                  SizedBox(height:MediaQuery.of(context).size.height*0.005,),
+                                Row(
+                                  children: [
+                                    Text('서울 중구 저동1가'
+                                      ,style: TextStyle(
+                                        //fontWeight: FontWeight.bold,
+                                        fontSize: 10,
+                                        color: Colors.black,
+                                      ),),
+                                    SizedBox(width:MediaQuery.of(context).size.width*0.6,),
+                                    Text('0.8km'
+                                      ,style: TextStyle(
+                                        //fontWeight: FontWeight.bold,
+                                        fontSize: 10,
+                                        color: Colors.black,
+                                      ),)
+                                  ],
+                                ),
+                                ]
                               ),
-                              ]
                             ),
                           ],
                         ),
