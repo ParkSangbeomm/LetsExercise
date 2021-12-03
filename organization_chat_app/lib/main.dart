f972e87114879912c222f319b186098b0566a4da
yhoon5171 <65946205+yhoon5171@users.noreply.github.com>
2021-12-03 23:51:05 +0900

    added google signin 2021/12/03

--- organization_chat_app/lib/main.dart
+++ organization_chat_app/lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(ExerciseApp());
 }
 
